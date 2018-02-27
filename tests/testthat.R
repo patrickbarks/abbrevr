c983cafdab6f6cc90581027b58b6658330f7dd03
patrickbarks <patrick.barks@gmail.com>
2018-02-27 10:50:03 +0100

    Moved tests from testthat.R to own file

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,12 +2,3 @@ library(testthat)
 library(abbrevr)
 
 test_check("abbrevr")
-
-test_that("Correct abbreviation for 'easy' English-language titles", {
-  expect_equal(AbbrevTitle("Science"), "Science")
-  expect_equal(AbbrevTitle("Journal of Ecology"), "J. Ecol.")
-  expect_equal(AbbrevTitle("Journal of Evolutionary Biology"), "J. Evol. Biol.")
-  expect_equal(AbbrevTitle("American Journal of Epidemiology"), "Am. J. Epidemiol.")
-  expect_equal(AbbrevTitle("Annual Review of Ecology and Systematics"), "Annu. Rev. Ecol. Syst.")
-  expect_equal(AbbrevTitle("PLoS ONE"), "PLoS ONE")
-})
