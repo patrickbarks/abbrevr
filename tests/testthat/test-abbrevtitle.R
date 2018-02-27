3085f996306ccf43bb9f56bb8ee8a5cc7aeb29bf
patrickbarks <patrick.barks@gmail.com>
2018-02-27 15:53:02 +0100

    Add tests for abbreviating titles with multi-word matches

--- tests/testthat/test-abbrevtitle.R
+++ tests/testthat/test-abbrevtitle.R
@@ -11,3 +11,14 @@ test_that("Correct abbreviation for easy English-language titles", {
   expect_equal(AbbrevTitle("Annual Review of Ecology and Systematics"), "Annu. Rev. Ecol. Syst.")
   expect_equal(AbbrevTitle("PLoS ONE"), "PLoS ONE")
 })
+
+test_that("Correct abbreviation for titles with matching multi-word phrases", {
+  expect_equal(AbbrevTitle("The Journal of the Anthropological Institute of Great Britain and Ireland"),
+               "J. Anthropol. Inst. G. B. Irel.")
+  expect_equal(AbbrevTitle("Journal of North American Herpetology"),
+               "J. N. Am. Herpetol.")
+  expect_equal(AbbrevTitle("Journal of Great Lakes Research"),
+               "J. Gt. Lakes Res.")
+  expect_equal(AbbrevTitle("Transactions of the New York Academy of Sciences"),
+               "Trans. N. Y. Acad. Sci.")
+})
